61c315d81dfe56a7b831d0a20ea2ec6ac79fd412
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-04-04 10:33:24 -0300

    Aula 15, usando arrow function, primeiro componente, expressão js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Text} from 'react-native'
+import {Text, View} from 'react-native'
 
-function App(){
-  return <Text>Meu primeiro componente</Text>
-}
+import Primeiro from './components/Primeiro'
 
-export default App;
+export default () => (
+  <View>    
+    <Primeiro/>
+  </View>
+)
---
+++ src/components/Primeiro.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default () => <Text>Primeiro!{1+3} somando usando expressão js</Text>
