76fb94795cc62480681c4b9464a15a9ee9fd0d5f
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-04-15 08:46:57 -0300

    Added return to snippet

---
+++ src/components/direta/Filho.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text,} from 'react-native';
+import styles from '../common/globalStyle'
+
+export default props => {
+    return(
+        <>
+            <Text style={styles.fontG}>
+                {props.a}
+            </Text>
+            <Text style={styles.fontG}>
+                {props.b}
+            </Text>        
+        </>
+    )
+}
